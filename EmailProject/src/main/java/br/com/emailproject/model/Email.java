0bf13480331f84431010cbdee279f3658b3a50d3
Douglas <Douglas@Douglas>
2022-05-12 13:31:40 -0300

    teste

--- EmailProject/src/main/java/br/com/emailproject/model/Email.java
+++ EmailProject/src/main/java/br/com/emailproject/model/Email.java
@@ -1,5 +1,33 @@
 package br.com.emailproject.model;
 
 public class Email {
+	
+	private String desinatario;
+	private String assunto;
+	private String texto;
+
+	public String getDesinatario() {
+		return desinatario;
+	}
+
+	public void setDesinatario(String desinatario) {
+		this.desinatario = desinatario;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
 
 }
